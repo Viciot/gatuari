91cdf0d9dd6bb924280272d1c4ad15206917190e
Victor Fuentes <vfuentesnoguera@gmail.com>
2021-09-16 12:01:26 +0200

    post newcat

--- server/routes/cat.route.js
+++ server/routes/cat.route.js
@@ -16,3 +16,14 @@ router.get(('/adoption'), (req, res) => {
   })
   .catch( err => res.json(err))
 })
+
+//  code done on 16/9/21 test_1
+router.post(('/newcat'), (req, res) => {
+  const {name, image, race, size, birth_date, arrival_date, color, country, city, register_number, description} = req.body;
+  Cat.create({name, image, race, size, birth_date, arrival_date, color, country, city, register_number, description})
+    .then(newCat=>{
+      console.log("Cat created, newCat")
+      res.redirect("/")
+    })
+    .catch(err=> console.log(err))   
+});
